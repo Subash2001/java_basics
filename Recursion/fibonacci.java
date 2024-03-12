c5458ad921fcf155c3b7f30ba64e0008fce46c67
Subash2001 <subash662009@gmail.com>
2024-03-12 17:46:33 +0530

    Recursion: Add Math formula to solve fibonacci

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -4,12 +4,20 @@ public class fibonacci {
     public static void main(String[] args) {
         int ans = Fibonacci(5);
         System.out.println(ans);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(FibonacciFormula(i));
+        }
     }
-    static int Fibonacci(int n){
+
+    static int FibonacciFormula (int n){
+        // use long instead
+       return (int)(Math.pow(((1+Math.sqrt(5)) /2 ), n) / Math.sqrt(5));
+    }
+    static int Fibonacci(int n) {
         // base condition
-        if (n < 2){
+        if (n < 2) {
             return n;
         }
-        return Fibonacci(n-1) + Fibonacci(n-2);
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }
